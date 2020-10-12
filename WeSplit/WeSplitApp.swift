49ff313741d5812417bf96c704f021aeeb8deced
Pavel Sakhanko <iospaveldev@gmail.com>
2020-10-12 23:00:31 +0300

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Pavel Sakhanko on 12.10.20.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
