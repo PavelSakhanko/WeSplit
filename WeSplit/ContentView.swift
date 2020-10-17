a31451c512330156916ac78fdd965f1c5fea4f8b
Pavel Sakhanko <iospaveldev@gmail.com>
2020-10-17 11:16:23 +0300

    feat: added basic ui

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,9 +8,47 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var checkAmount = ""
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 2
+    
+    let tipPercentages = [10, 15, 20, 25, 0]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Amount", text: $checkAmount)
+                    
+                    Picker("Number of people", selection: $numberOfPeople) {
+                        ForEach(2 ..< 100) {
+                            Text("\($0) people")
+                        }
+                    }
+                }
+                
+                Section(header: Text("How much tip do you want to leave?")) {
+                    Picker("Tip percentage", selection: $tipPercentage) {
+                        ForEach(0 ..< tipPercentages.count) {
+                            Text("\(self.tipPercentages[$0])%")
+                        }
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                }
+
+                Section {
+                    Text("$\(checkAmount)")
+                }
+            }
+            .navigationBarTitle("We Split")
+            
+            HStack(alignment: .center, spacing: 30, content: {
+                Spacer()
+                TextField("Amount", text: $checkAmount)
+                    .keyboardType(.decimalPad)
+            })
+        }
     }
 }
 
